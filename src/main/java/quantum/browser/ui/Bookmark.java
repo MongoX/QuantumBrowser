718ce2bdfcec1a9c7c4642568a99e22f773d2507
Quantum <quantum2048@gmail.com>
2015-06-07 16:00:15 -0400

    Why do I have to use AWT menus?

--- src/main/java/quantum/browser/ui/Bookmark.java
+++ src/main/java/quantum/browser/ui/Bookmark.java
@@ -3,8 +3,10 @@ package quantum.browser.ui;
 import quantum.browser.data.Bookmarks;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 
 public class Bookmark extends JMenuItem implements ActionListener {
     private final TabManager manager;
@@ -17,6 +19,19 @@ public class Bookmark extends JMenuItem implements ActionListener {
         this.name = name;
         this.folder = folder;
         addActionListener(this);
+    }
+
+    @Override
+    protected void processMouseEvent(MouseEvent e) {
+        if (SwingUtilities.isRightMouseButton(e)) {
+            final PopupMenu menu = new PopupMenu(name);
+            add(menu);
+            menu.add(new MenuItem("Delete") {{
+                folder.delete(name);
+            }});
+            menu.show(this, e.getX(), e.getY());
+        } else
+            super.processMouseEvent(e);
     }
 
     @Override
