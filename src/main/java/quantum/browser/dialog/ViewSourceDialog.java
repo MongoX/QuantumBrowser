c36cc09b82ebd26d6734872f0914fbf21f9d16f6
Quantum <quantum2048@gmail.com>
2015-06-08 21:51:23 -0400

    Pray this fixes the view source hanging

--- src/main/java/quantum/browser/dialog/ViewSourceDialog.java
+++ src/main/java/quantum/browser/dialog/ViewSourceDialog.java
@@ -6,6 +6,9 @@ import org.cef.browser.CefBrowser;
 import org.cef.browser.CefMessageRouter;
 import org.cef.callback.CefQueryCallback;
 import org.cef.handler.CefMessageRouterHandlerAdapter;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
 import quantum.browser.handler.AppHandler;
 import quantum.browser.natives.NativeLoader;
 import quantum.browser.ui.MainFrame;
@@ -13,26 +16,24 @@ import quantum.browser.utils.Resources;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.InputStream;
 import java.net.URL;
 
 
-public class ViewSourceDialog extends JFrame implements Runnable {
-    public ViewSourceDialog(String url, final String content) {
-        super("View Source: " + url);
+public class ViewSourceDialog extends JDialog implements Runnable {
+    public ViewSourceDialog(MainFrame owner, String url, final String content) {
+        super(owner, "View Source: " + url, false);
         setLayout(new BorderLayout());
         setSize(new Dimension(640, 480));
-
-        CefClient client = CefApp.getInstance().createClient();
-        client.addMessageRouter(CefMessageRouter.create(new CefMessageRouterHandlerAdapter() {
-            @Override
-            public boolean onQuery(CefBrowser browser, long query_id, String request, boolean persistent, CefQueryCallback callback) {
-                callback.success(content);
-                return true;
-            }
-        }));
-        CefBrowser browser = client.createBrowser("quantum://resources/prism.html", false, false);
-        add(browser.getUIComponent(), BorderLayout.CENTER);
+        RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_HTML);
+        textArea.setCodeFoldingEnabled(true);
+        textArea.setEditable(false);
+        textArea.setText(content);
+        add(new RTextScrollPane(textArea));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 
     @Override
@@ -41,15 +42,12 @@ public class ViewSourceDialog extends JFrame implements Runnable {
     }
 
     public static void main(String... args) throws Exception {
-        NativeLoader.unpack();
-        CefApp.addAppHandler(new AppHandler());
-
         System.out.print("Loading...");
         InputStream source = new URL("http://en.wikipedia.org/wiki/Main_Page").openStream();
         String html = Resources.toString(source);
         System.out.println(" Done");
         System.out.println(html);
         System.out.println(html.length());
-        SwingUtilities.invokeLater(new ViewSourceDialog("http://en.wikipedia.org/wiki/Main_Page", html));
+        SwingUtilities.invokeLater(new ViewSourceDialog(null, "http://en.wikipedia.org/wiki/Main_Page", html));
     }
 }
--- src/main/java/quantum/browser/ui/Tab.java
+++ src/main/java/quantum/browser/ui/Tab.java
@@ -284,7 +284,7 @@ public class Tab extends JPanel {
                         browser.getSource(new CefStringVisitor() {
                             @Override
                             public void visit(String string) {
-                                new Thread(new ViewSourceDialog(url, string)).start();
+                                SwingUtilities.invokeLater(new ViewSourceDialog(manager.owner, url, string));
                             }
                         });
                         return true;
